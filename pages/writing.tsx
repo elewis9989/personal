bfdf96ba92a2658166c7b7a49abe23921aedaeaa
elijah <elewis9989@gmail.com>
2022-08-06 23:02:58 -0400

    create page layout

--- pages/writing.tsx
+++ pages/writing.tsx
@@ -4,9 +4,16 @@ import Layout from '../components/Layout';
 const Writing: NextPage = () => {
   return (
     <Layout>
+      <div className='flex items-center justify-center'>
+        <h1 className='title pb-4'>Writing ✍️</h1>
+      </div>
       <div className='flex flex-col'>
-        <h1 className='text-5xl pb-4'>🌻 Poetry</h1>
-        <p className='italic ml-4'>...working on it!</p>
+        <h1 className='header-1 py-4'>🌻 Poetry</h1>
+        <p className='header-3 italic ml-4'>...working on it!</p>
+        <h1 className='header-1 py-4'>⚙️ Tech Articles</h1>
+        <p className='header-3 italic ml-4'>...working on it!</p>
+        <h1 className='header-1 py-4'>👽 Blog</h1>
+        <p className='header-3 italic ml-4'>...working on it!</p>
       </div>
     </Layout>
   );
