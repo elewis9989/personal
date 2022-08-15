317705cf0cc77adb170004013e809376ed8b8b59
elijah <elewis9989@gmail.com>
2022-08-15 16:56:03 -0400

    basic layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
 import Navbar from './Navbar';
+import Newsletter from './Newletter';
 import Profile from './Profile';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -14,6 +15,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <main className='flex-1 pb-14 pt-14 md:pt-28 lg:px-24 px-8'>
         <Profile />
         <div className='pt-14'>{children}</div>
+        <Newsletter />
       </main>
       <footer className='flex items-center justify-center md:mx-52 mx-14 border-t pt-2 pb-6'>
         <a href='#' target='_self' rel='noopener noreferrer'>
---
+++ utils/useSubscribeToNewsletter.ts
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from 'react';
+import { Form, FormState } from './Form';
+
+export function useSubscribeToNewsletter() {
+  const [form, setForm] = useState<FormState>({ state: Form.Initial });
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  async function subscribe(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setForm({ state: Form.Loading });
+
+    // make a request
+    const res = await fetch('/api/subscribe', {
+      body: JSON.stringify({
+        email: inputEl.current!.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+
+    // check to see if there was error
+    const { error } = await res.json();
+
+    if (error) {
+      setForm({
+        state: Form.Error,
+        message: error,
+      });
+
+      return;
+    }
+
+    if (inputEl.current?.value !== undefined) {
+      inputEl.current.value = '';
+    }
+
+    setForm({
+      state: Form.Success,
+      message: `Success! 🎉 You've been added to the list.`,
+    });
+  }
+
+  return { subscribe, inputEl, form };
+}
