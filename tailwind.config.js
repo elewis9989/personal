b833702db9aea0b98e907d326aac5cb252e86fa9
elewis9989 <elewis9989@gmail.com>
2023-02-08 15:32:27 -0500

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  // Ensure these match with .storybook/preview.js
+  theme: {
+    screens: {
+      '3xs': '320px',
+      '2xs': '375px',
+      xs: '425px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+      '2xl': '1280px',
+      '3xl': '1536px',
+    },
+  },
+  plugins: [],
+};
